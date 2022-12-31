07a1509bc28a47a61ea8e236ab12aebd175b278f
DenPC <zmaev.vsu@gmail.com>
2022-12-31 15:31:18 +0300

    Add menu

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # GigaQuiz
+<h1> GigaQuiz <h1>
+<p> GigaQuiz is a simple android app where user can take a quiz on various topics</p>
--- app/src/main/java/ru/vsu/cs/zmaev/MainActivity.java
+++ app/src/main/java/ru/vsu/cs/zmaev/MainActivity.java
@@ -1,14 +1,20 @@
 package ru.vsu.cs.zmaev;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import ru.vsu.cs.zmaev.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
