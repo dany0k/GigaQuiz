b722fa6bbb17f6341bdee7de975414aa1e385c01
DenPC <zmaev.vsu@gmail.com>
2023-01-12 15:16:26 +0300

    Add more questions with images

--- app/src/main/java/ru/vsu/cs/zmaev/GeographicalGameFragment.java
+++ app/src/main/java/ru/vsu/cs/zmaev/GeographicalGameFragment.java
@@ -1,5 +1,7 @@
 package ru.vsu.cs.zmaev;
 
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,33 +18,74 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 import ru.vsu.cs.zmaev.databinding.FragmentGeographicalGameBinding;
 import ru.vsu.cs.zmaev.model.AnswersViewModel;
 
 public class GeographicalGameFragment extends Fragment {
 
+    FragmentGeographicalGameBinding binding = null;
+
     public AnswersViewModel sharedViewModel;
     public GeographicalGameFragment() {
         super(R.layout.fragment_geographical_game);
     }
 
-    List<Question> questions = Arrays.asList(
-            new Question("Какое самое глубокое озеро в мире?",
+    List<ImageQuestion> questions = Arrays.asList(
+            new ImageQuestion("Какое самое глубокое озеро в мире?",
                     new ArrayList<>(Arrays.asList(
                             "Байкал", "Каспийское море", "Виктория", "Мичиган")
-                    )),
-            new Question("Столица Албании?",
+                    ),
+                    R.drawable.earth),
+            new ImageQuestion("Столица Албании?",
                     new ArrayList<>(Arrays.asList(
                             "Тирана", "София", "Загреб", "Братислава")
-                    )),
-            new Question("Какой стране принадлежит этот флаг?",
+                    ),
+                    R.drawable.earth),
+            new ImageQuestion("Какой стране принадлежит этот флаг?",
                     new ArrayList<>(Arrays.asList(
                             "Аргентина", "Бельгия", "Уругвай", "Либерия")
-                    ))
+                    ),
+                    R.drawable.argentina_flag),
+            new ImageQuestion("Какая самая высокая гора в Европе?",
+                    new ArrayList<>(Arrays.asList(
+                            "Эльбрус", "Эверест", "Шхара", "Монбланч")
+                    ),
+                    R.drawable.mountain),
+            new ImageQuestion("Какой самый крупный город по населению Европе?",
+                    new ArrayList<>(Arrays.asList(
+                            "Стамбул", "Лондон", "Москва", "Париж")
+                    ),
+                    R.drawable.city),
+            new ImageQuestion("Какой стране принадлежит этот флаг?",
+                    new ArrayList<>(Arrays.asList(
+                            "Аргентина", "Бельгия", "Уругвай", "Либерия")
+                    ),
+                    R.drawable.argentina_flag),
+            new ImageQuestion("Какой стране является третьей по площади в мире?",
+                    new ArrayList<>(Arrays.asList(
+                            "США", "Китай", "Канада", "Бразилия")
+                    ),
+                    R.drawable.earth),
+            new ImageQuestion("Какая страна изображена на силуете?",
+                    new ArrayList<>(Arrays.asList(
+                            "Австрия", "Словения", "Бельгия", "Болгария")
+                    ),
+                    R.drawable.austria),
+            new ImageQuestion("Какая страна является самой малой по площади в Южной Америке?",
+                    new ArrayList<>(Arrays.asList(
+                            "Суринам", "Гайана", "Французская Гвиана", "Уругвай")
+                    ),
+                    R.drawable.earth),
+            new ImageQuestion("Какая страна является бывшей колонией Португалии в Китае?",
+                    new ArrayList<>(Arrays.asList(
+                            "Макао", "Гонконг", "Тайвань", "Тибет")
+                    ),
+                    R.drawable.earth)
     );
 
-    public Question currentQuestion;
+    public ImageQuestion currentQuestion;
     public String correctAnswer;
     public List<String> answers;
     public int questionIndex = 0;
@@ -56,7 +99,7 @@ public class GeographicalGameFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        FragmentGeographicalGameBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_geographical_game, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_geographical_game, container, false);
         sharedViewModel = new ViewModelProvider(getActivity()).get(AnswersViewModel.class);
         randomizeQuestions();
         binding.setGame(GeographicalGameFragment.this);
@@ -80,7 +123,7 @@ public class GeographicalGameFragment extends Fragment {
                 } else {
                     incorrectAnswersCounter++;
                 }
-                if (questionIndex < questionNum) {
+                if (questionIndex < questions.size() - 1) {
                     questionIndex++;
                     currentQuestion = questions.get(questionIndex);
                     setQuestion();
@@ -104,6 +147,7 @@ public class GeographicalGameFragment extends Fragment {
 
     private void setQuestion() {
         currentQuestion = questions.get(questionIndex);
+        binding.questionImage.setImageResource(currentQuestion.getDrawable_id());
         correctAnswer = currentQuestion.getAnswers().get(0);
         answers = questions.get(questionIndex).getAnswers();
         Collections.shuffle(answers);
---
+++ app/src/main/java/ru/vsu/cs/zmaev/ImageQuestion.java
@@ -0,0 +1,27 @@
+package ru.vsu.cs.zmaev;
+
+import java.util.List;
+
+public class ImageQuestion {
+    private final String text;
+    private final List<String> answers;
+    private final int drawable_id;
+
+    public ImageQuestion(String text, List<String> answers, int drawable_id) {
+        this.text = text;
+        this.answers = answers;
+        this.drawable_id = drawable_id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public List<String> getAnswers() {
+        return answers;
+    }
+
+    public int getDrawable_id() {
+        return drawable_id;
+    }
+}
