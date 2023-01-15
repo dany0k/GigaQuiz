e0a91a19067bb1807f04e3e0f2786658f192fe48
DenPC <zmaev.vsu@gmail.com>
2023-01-15 18:02:29 +0300

    Move profile classes to module pakage

--- app/src/main/java/ru/vsu/cs/zmaev/model/ImageQuestion.java
+++ app/src/main/java/ru/vsu/cs/zmaev/model/ImageQuestion.java
@@ -1,4 +1,4 @@
-package ru.vsu.cs.zmaev;
+package ru.vsu.cs.zmaev.model;
 
 import java.util.List;
 
--- app/src/main/java/ru/vsu/cs/zmaev/model/User.java
+++ app/src/main/java/ru/vsu/cs/zmaev/model/User.java
@@ -1,4 +1,4 @@
-package ru.vsu.cs.zmaev;
+package ru.vsu.cs.zmaev.model;
 
 public class User {
 
