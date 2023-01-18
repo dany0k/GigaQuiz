b015bab470442bd92c3b415a60290f3c8b323cfa
DenPC <zmaev.vsu@gmail.com>
2023-01-18 14:59:38 +0300

    Update values in Profiles fragment

--- app/src/main/java/ru/vsu/cs/zmaev/UserProfileFragment.java
+++ app/src/main/java/ru/vsu/cs/zmaev/UserProfileFragment.java
@@ -2,10 +2,13 @@ package ru.vsu.cs.zmaev;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -23,6 +26,10 @@ public class UserProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_user_profile, container, false);
+        binding.editProfileButton.setOnClickListener(v -> {
+            Navigation.findNavController(v).navigate(R.id.action_userProfileFragment_to_userEditProfileFragment);
+        });
         return binding.getRoot();
     }
+
 }
