a77fbc349c281a780336fc7690374ba79160b4d3
DenPC <zmaev.vsu@gmail.com>
2023-01-21 15:05:07 +0300

    Delete rudinant fragments

--- app/src/main/java/ru/vsu/cs/zmaev/UserProfileFragment.java
+++ app/src/main/java/ru/vsu/cs/zmaev/UserProfileFragment.java
@@ -12,8 +12,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import ru.vsu.cs.zmaev.databinding.FragmentUserProfileBinding;
 import ru.vsu.cs.zmaev.tools.JSONTools;
